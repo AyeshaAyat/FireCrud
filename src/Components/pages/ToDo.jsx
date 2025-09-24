be1748c36fec2b687685c237176bdd6891c0afdb
AyeshaAyat <ayeshaayat184@gmail.com>
2025-09-24 10:35:29 +0600

    editstart

--- src/Components/pages/ToDo.jsx
+++ src/Components/pages/ToDo.jsx
@@ -6,18 +6,18 @@ const ToDo = () => {
    <div className="bg-blue-300 py-40">
 
 
-        <form className="max-w-sm mx-auto">
+        <div className="max-w-sm mx-auto">
           <div className="mb-5">
             <label
               htmlFor="email"
-              className="block mb-2 text-[18px] ml-1.5 font-medium text-gray-900 dark:text-white"
+              className="block mb-2.5 text-[21px] text-center  italic font-medium text-gray-900 dark:text-white"
               >
-              Add Task
+              To-Do List
             </label>
             <input
               type="email"
               id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border text-center border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="add task here"
              
               />
@@ -25,11 +25,11 @@ const ToDo = () => {
 
           <button
             type="submit"
-            className="text-white bg-yellow-400 hover:bg-pink-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-yellow-400 hover:bg-pink-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-small w-full sm:w-auto px-41 ml-0.5 py-2.5  text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
             Submit
           </button>
-        </form>
+        </div>
            
       </div>
    </>
